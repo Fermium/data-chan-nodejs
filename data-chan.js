b4a6adeb87a5432bdc74b4d8665133cef06f1df1
sfermiumlabs <s@fermiumlabs.com>
2017-04-04 14:14:59 +0200

    fixed path to lib

--- data-chan.js
+++ data-chan.js
@@ -24,7 +24,7 @@ var datachan_acquire_result_t = struct({
 
 
 
-module.exports.lib =  ffi.DynamicLibrary(__dirname+'/libDataChan',{
+module.exports.lib =  ffi.DynamicLibrary('./libDataChan',{
   'datachan_is_initialized' : [ref.types.bool,[]],
   'datachan_init' : [ref.types.void,[]],
   'datachan_shutdown' : [ref.types.void,[]],
